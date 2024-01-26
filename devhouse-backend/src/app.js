e1314188d332e0a15914a4a8c6bd89a11b2f339c
Victor Bratfisch <victorbratfisch22@gmail.com>
2024-01-26 11:39:23 -0300

    mongoDB connect

--- devhouse-backend/src/app.js
+++ devhouse-backend/src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose'
 import routes from './routes';
 
 class App{
 
     constructor(){
       this.server= express();
+
+      mongoose.connect('mongodb+srv://victorbratfisch:devhouse@devhouse.n8fjxgg.mongodb.net/devhouse?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      
       this.middlewares();
       this.routes();
     }
