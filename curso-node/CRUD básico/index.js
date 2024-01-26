554a78a4283bbee59494ce70461531df29225f96
Victor Bratfisch <victorbratfisch22@gmail.com>
2024-01-26 11:16:53 -0300

    first commit node js udemy

---
+++ curso-node/CRUD básico/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ['Node', 'JavaScript', 'Flutter'];
+
+//Middleware global
+server.use((req, res, next) => {
+  console.log(`URL CHAMADA: ${req.url}`)
+
+  return next();
+});
+
+function checkCurso(req, res, next){
+  if(!req.body.name){
+    return res.status(400).json({error: 'Nome do curso é obrigatório'})
+  }
+
+  return next();
+}
+
+function checkIndexCurso(req, res, next){
+  const curso = cursos[req.params.index];
+  if(!curso){
+    return res.status(400).json({ error: 'O curso não existe' })
+  }
+
+  req.curso = curso;
+
+  return next();
+}
+
+server.get('/cursos', (req, res)=> {
+  return res.json(cursos)
+});
+
+server.get('/cursos/:index', checkIndexCurso, (req, res) => {
+
+  return res.json(req.curso);
+
+});
+
+server.post('/cursos', checkCurso, (req, res)=>{
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res)=> {
+  const { index } = req.params;
+  const { name } =  req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos)
+})
+
+
+server.delete('/cursos/:index', checkIndexCurso, (req, res)=> {
+  const { index } = req.params;
+
+  cursos.splice(index, 1)
+
+  return res.json(cursos)
+})
+
+server.listen(3000);
