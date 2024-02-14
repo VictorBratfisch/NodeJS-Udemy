ff97a6f0e237db1330d580a0885533b28f1afc4c
Victor Bratfisch <victorbratfisch22@gmail.com>
2024-02-14 15:56:52 -0300

    finish devhouse

--- devhouse-backend/src/controllers/HouseController.js
+++ devhouse-backend/src/controllers/HouseController.js
@@ -1,5 +1,6 @@
-import House from '../models/House'
+import House from '../models/House';
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class HouseController {
 
@@ -12,9 +13,21 @@ class HouseController {
   }
 
   async store(req, res) {
+
+    const schema = Yup.object().shape({
+      description: Yup.string().required(),
+      price: Yup.number().required(),
+      location: Yup.string().required(),
+      status: Yup.boolean().required()
+    })
+
     const  { filename } = req.file;
     const  { description, price, location, status } = req.body;
     const { user_id } = req.headers;
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ error: "Falha na validação." })
+    }
 
     const house = await House.create({
         user: user_id,
--- devhouse-backend/src/routes.js
+++ devhouse-backend/src/routes.js
@@ -4,6 +4,7 @@ import HouseController from "./controllers/HouseController";
 import multer from "multer";
 import uploadConfig from "./config/upload";
 import DashboardController from "./controllers/DashboardController";
+import ReserveController from "./controllers/ReserveController";
 
 const routes = new Router();
 
@@ -17,5 +18,9 @@ routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.upda
 routes.delete('/houses', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store)
+routes.get('/reserves', ReserveController.index)
+routes.delete('/reserves/cancel', ReserveController.destroy)
 
 export default routes;
