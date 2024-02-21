0e3e5519590c7ac94fe6db71e30c9b4fa1e75fd6
Victor Bratfisch <victorbratfisch22@gmail.com>
2024-02-20 21:21:10 -0300

    integrate postgreSQL

--- tasklist/src/app.js
+++ tasklist/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import routes from "./routes";
 
+import './database/index'
+
 class App {
   constructor() {
     this.server = express();
--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,3 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/teste', async (req, res) => {
+    const user = await User.create({ 
+        name: 'Victor', 
+        email: 'victor@teste.com',
+        password_hash: '123123', 
+    })
+    return res.json(user)
+})
+
+export default routes;
