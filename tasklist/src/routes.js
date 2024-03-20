65760c180cbfd2af8862420350a3d0204e34c3c3
Victor Bratfisch <victorbratfisch22@gmail.com>
2024-03-20 14:03:17 -0300

    finish devhouse

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-    const user = await User.create({ 
-        name: 'Victor', 
-        email: 'victor@teste.com',
-        password_hash: '123123', 
-    })
-    return res.json(user)
-})
+routes.post('/users', UserController.store);
 
 export default routes;
